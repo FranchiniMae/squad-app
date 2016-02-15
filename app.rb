ea7fbe3aaf491912573f2a2484a36f579990b8ef
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL_ADDRESS>
2016-02-14 20:30:04 -0800

    Adds more routes and css

--- app.rb
+++ app.rb
@@ -1,12 +1,60 @@
 class SquadApp < Sinatra::Base
 	
+	#home page before seeing ideas
 	get '/' do
-		"Hello, World!"
+		@ideas = Idea.all
+		erb(:"home")
 	end
 
+	#index 
 	get '/ideas' do
 		@ideas = Idea.all
-		erb :'index'
+		erb(:"index")
+	end
+
+	#create
+	get '/ideas/new' do
+		erb(:"ideas/new")
+	end
+
+	#create
+	post '/ideas' do
+		@idea = Idea.create(params[:idea])
+		@idea.save
+		redirect("/ideas")
+	end
+	#took out "s" from @idea
+
+	#show
+	get '/ideas/:id' do
+		@idea = Idea.find(params[:id])
+		erb(:"show")
+	end
+
+	#edit
+	get '/ideas/:id/edit' do
+		@idea = Idea.find(params[:id])
+		erb(:"ideas/edit")
+	end
+
+	#update
+	put 'ideas/:id' do
+		@idea = Idea.find(params[:id])
+		if @idea.update_attributes(params[:idea])
+			redirect('/ideas/#{idea.id}')
+		else
+			erb(:"ideas/edit")
+		end
+	end 
+
+	#delete
+	delete '/ideas/:id/delete' do
+		@idea = Idea.find(params[:id])
+		if @idea.destroy
+		  redirect("/ideas")
+		else 
+		  redirect("/ideas/#{@idea.id}")
+		end 
 	end
 
 end
